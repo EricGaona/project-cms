c0690f65e259096737cebeccd2e0cc9ff722451d
Eric Gaona <ericgaona0413@gmail.com>
2021-04-01 14:18:08 +0100

    adding the front end in Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
+use App\Http\Controllers\FrontEndController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,6 @@ Auth::routes();
 Route::view('/', 'welcome');
 
 Route::get('/inicio', [InicioController::class, 'index']);
+
+//  Front End
+Route::get('/', [FrontEndController::class, 'inicio']);
